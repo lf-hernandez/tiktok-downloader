11b4c2d6ca6e712ceffecb85c48f3882784c5fc9
Luis Felipe Hernandez <luis.hernandez093@gmail.com>
2023-09-18 17:01:52 +0100

    Add simple web server

--- main.py
+++ main.py
@@ -5,28 +5,43 @@ import logging
 import os
 
 import aiohttp
+from aiohttp import web
 from tiktokapipy.async_api import AsyncTikTokAPI
 from tiktokapipy.models.video import Video
 
-directory = './videos'
+directory = "./videos"
 
 
 async def save_file(api: AsyncTikTokAPI, url: str, filename: str):
-    async with aiohttp.ClientSession(cookies={cookie["name"]: cookie["value"] for cookie in await api.context.cookies() if cookie["name"] == "tt_chain_token"}) as session:
-        async with session.get(url, headers={"referer": "https://www.tiktok.com/"}) as resp:
-            downloaded = io.BytesIO(await resp.read())
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+
+    async with aiohttp.ClientSession(
+        cookies={
+            cookie["name"]: cookie["value"]
+            for cookie in await api.context.cookies()
+            if cookie["name"] == "tt_chain_token"
+        }
+    ) as session:
+        async with session.get(
+            url, headers={"referer": "https://www.tiktok.com/"}
+        ) as response:
+            binary_response = await response.read()
+            bytes_stream = io.BytesIO(binary_response)
             with open(os.path.join(directory, filename), "wb") as file:
-                file.write(downloaded.getbuffer())
+                file.write(bytes_stream.getbuffer())
 
 
 async def save_slideshow(video: Video, api: AsyncTikTokAPI):
-    vf = "\"scale=iw*min(1080/iw\,1920/ih):ih*min(1080/iw\,1920/ih)," \
-         "pad=1080:1920:(1080-iw)/2:(1920-ih)/2," \
-         "format=yuv420p\""
+    vf = (
+        '"scale=iw*min(1080/iw\,1920/ih):ih*min(1080/iw\,1920/ih),'
+        "pad=1080:1920:(1080-iw)/2:(1920-ih)/2,"
+        'format=yuv420p"'
+    )
 
-    for i, image_data in enumerate(video.image_post.images):
+    for index, image_data in enumerate(video.image_post.images):
         url = image_data.image_url.url_list[-1]
-        await save_file(api, url, f"{video.id}_{i:02}.jpg")
+        await save_file(api, url, f"{video.id}_{index:02}.jpg")
 
     await save_file(api, video.music.play_url, f"{video.id}.mp3")
 
@@ -40,7 +55,7 @@ async def save_slideshow(video: Video, api: AsyncTikTokAPI):
         "-acodec copy",
         f"-t {len(video.image_post.images) * 2.5}",
         f"{directory}/{video.id}.mp4",
-        "-y"
+        "-y",
     ]
     ffmpeg_proc = await asyncio.create_subprocess_shell(
         " ".join(command),
@@ -69,7 +84,7 @@ async def download_video(video):
             await save_video(video, api)
 
 
-async def get_hashtag_videos(hashtag):
+async def get_videos_by_hashtag(hashtag):
     async with AsyncTikTokAPI() as api:
         challenge = await api.challenge(hashtag, video_limit=3)
         async for video in challenge.videos:
@@ -77,4 +92,24 @@ async def get_hashtag_videos(hashtag):
 
 
 if __name__ == "__main__":
-    asyncio.run(get_hashtag_videos(hashtag="github"))
+    routes = web.RouteTableDef()
+
+    @routes.get("/")
+    async def get_handler(request):
+        return web.Response(status=200, text="Web server is up and running!")
+
+    @routes.post("/download")
+    async def post_handler(request):
+        params = request.query
+        hashtag = params.get("hashtag")
+        if not hashtag:
+            return web.HTTPError()
+
+        await get_videos_by_hashtag(hashtag)
+
+        return web.Response(status=200)
+
+    app = web.Application()
+    app.add_routes(routes)
+    logging.basicConfig(level=logging.DEBUG)
+    web.run_app(app, port=8000)
